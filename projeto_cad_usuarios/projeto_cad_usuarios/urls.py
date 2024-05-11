ee94b1662810caacc3212af23d3388a0220674d7
Marcos <marcosmoura201@hotmail.com>
2024-05-11 11:35:40 -0300

    Criando um projeto com python e django

---
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from app_cad_usuarios import views
+
+urlpatterns = [
+    #rota, view responsavel, nome de referncia
+    #usuarios.com
+    path('',views.home,name='home'),
+    # usuarios.com/usuarios
+    path('usuarios/', views.usuarios, name='listagem_usuarios')
+]
